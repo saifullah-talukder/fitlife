74c71034c40752112924c484f283487c9d88a963
Saifullah Talukder <100260341+saifullah-talukder@users.noreply.github.com>
2024-02-24 00:21:08 +0600

    add another plan button

--- src/components/planCard.tsx
+++ src/components/planCard.tsx
@@ -1,36 +1,39 @@
 import { useNewPlanResponseStore } from '@/stores/useNewPlanResponseStore'
-import { differenceInYears } from 'date-fns'
-import LabeledInformation from './labelledInformation'
+import { differenceInYears, format } from 'date-fns'
 import { capitalize } from 'lodash'
-import { format } from 'date-fns'
+import { PrimaryActionButton } from './button'
+import LabeledInformation from './labelledInformation'
 
 export default function PlanCard() {
-  const { response } = useNewPlanResponseStore()
+  const { response, setEmpty } = useNewPlanResponseStore()
 
   return (
-    <div className="w-full mt-4 grid grid-cols-3 gap-4 border border-fuchsia-300 p-4 rounded-lg">
-      <div className="space-y-4">
-        <LabeledInformation label="Age" value={differenceInYears(new Date(), new Date(response.params.birthDate!))} />
-        <LabeledInformation label="Sex" value={capitalize(response.params.sex)} />
-        <LabeledInformation label="Country" value={capitalize(response.params.country)} />
-        <LabeledInformation label="Height (cm)" value={capitalize(response.params.height)} />
-        <LabeledInformation label="Weight (kg)" value={capitalize(response.params.weight)} />
-        <LabeledInformation label="Goal" value={`${capitalize(response.params.goal)} weight`} />
-        <LabeledInformation label="Activity Level" value={capitalize(response.params.activityLevel)} />
-        <LabeledInformation label="Target Weight (kg)" value={capitalize(response.params.targetWeight)} />
-        <LabeledInformation label="Reason for Past Failure" value={capitalize(response.params.pastFailureReason)} />
-        <LabeledInformation label="Requested At" value={getFormattedTimeString(response.createdAt)} />
-      </div>
+    <div className="mt-4 w-full flex flex-col border border-fuchsia-300 p-4 rounded-lg">
+      <div className="w-full grid grid-cols-3 gap-4">
+        <div className="space-y-4">
+          <LabeledInformation label="Age" value={differenceInYears(new Date(), new Date(response.params.birthDate!))} />
+          <LabeledInformation label="Sex" value={capitalize(response.params.sex)} />
+          <LabeledInformation label="Country" value={capitalize(response.params.country)} />
+          <LabeledInformation label="Height (cm)" value={capitalize(response.params.height)} />
+          <LabeledInformation label="Weight (kg)" value={capitalize(response.params.weight)} />
+          <LabeledInformation label="Goal" value={`${capitalize(response.params.goal)} weight`} />
+          <LabeledInformation label="Activity Level" value={capitalize(response.params.activityLevel)} />
+          <LabeledInformation label="Target Weight (kg)" value={capitalize(response.params.targetWeight)} />
+          <LabeledInformation label="Reason for Past Failure" value={capitalize(response.params.pastFailureReason)} />
+          <LabeledInformation label="Requested At" value={getFormattedTimeString(response.createdAt)} />
+        </div>
 
-      <div>
-        <h2 className="font-medium text-slate-700 text-opacity-70">Your Meal Recommendation</h2>
-        <div className="mt-2 space-y-2">{stringToHtml(response.plan.meal!)}</div>
-      </div>
+        <div>
+          <h2 className="font-medium text-slate-700 text-opacity-70">Your Meal Recommendation</h2>
+          <div className="mt-2 space-y-2">{stringToHtml(response.plan.meal!)}</div>
+        </div>
 
-      <div>
-        <h2 className="font-medium text-slate-700 text-opacity-70">Your Workout Recommendation</h2>
-        <div className="mt-2 space-y-2">{stringToHtml(response.plan.workout!)}</div>
+        <div>
+          <h2 className="font-medium text-slate-700 text-opacity-70">Your Workout Recommendation</h2>
+          <div className="mt-2 space-y-2">{stringToHtml(response.plan.workout!)}</div>
+        </div>
       </div>
+      <PrimaryActionButton className="mt-8" label="Make Another Plan" onClick={setEmpty} />
     </div>
   )
 }
